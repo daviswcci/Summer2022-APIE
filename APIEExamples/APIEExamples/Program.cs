53e3e44eaefc0704d079940a41859734a7f7d74f
daviswcci <79462946+daviswcci@users.noreply.github.com>
2022-05-26 12:06:57 -0400

    Update Program.cs

--- APIEExamples/APIEExamples/Program.cs
+++ APIEExamples/APIEExamples/Program.cs
@@ -1,10 +1,66 @@
 ﻿using APIEExamples;
 
-// Encapsulation
-Game loz = new Game();
-loz.Play();
-Movie batman = new Movie();
-batman.Play();
-batman.Title = "The Batman";
-loz.ChangeTitle(batman.Title);
-batman.ChangeTitle("The Dark Knight Rises");
+// console application that acts like a media collection
+List<Media> collection = new List<Media>(); // list of all media objects we own
+
+// if we want to allow a user to look at the collection multiple times
+// loops! specifically while
+bool isRunning = true;
+while (isRunning)
+{
+    Console.Clear();
+    Console.WriteLine("What type of media would you like to add to the collection?");
+    Console.WriteLine("1 - Game \n2 - Movie");
+    Console.WriteLine("3 - Display Media");
+    int selection = Int32.Parse(Console.ReadLine()); // take in a user's input
+    switch (selection)
+    {
+        case 1:
+            // create a game
+            Console.Clear();
+            Game exampleGame = new Game();
+            Console.WriteLine("What is the name of your game?");
+            string gameTitle = Console.ReadLine();
+            exampleGame.ChangeTitle(gameTitle); // another way to write this is 'exampleGame.Title = gameTitle;'
+            exampleGame.Play();
+            // add it to the list
+            collection.Add(exampleGame);
+            break;
+        case 2:
+            // create a movie
+            Console.Clear();
+            Movie exampleMovie = new Movie();
+            Console.WriteLine("What is the name of your movie?");
+            string movieTitle = Console.ReadLine();
+            exampleMovie.ChangeTitle(movieTitle);
+            // add it to the list
+            collection.Add(exampleMovie);
+            break;
+        case 3:
+            // loop through our list
+            foreach(Media media in collection)
+            {
+                // Title: Halo - Hours Played: 10 - Game
+                // Title: Kill Bill - Movie
+                // challenge 1: how do we know what is a game vs. what is a movie
+                if (media.GetType() == typeof(Game)) // movie object is a game
+                {
+                    Game gameFromShelf = (Game)media; // type casting
+                    Console.WriteLine("Title: " + gameFromShelf.Title + " - Hours Played: " + gameFromShelf.TimePlayed + " - Game");
+                }
+                else if (media.GetType() == typeof(Movie)) // media object is a movie
+                {
+                    Console.WriteLine("Title: " + media.Title + " - Movie");
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+                
+                // challenge 2: how do we access child class specific properties from a parent class
+            }
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadLine();
+            break;
+    }
+}
